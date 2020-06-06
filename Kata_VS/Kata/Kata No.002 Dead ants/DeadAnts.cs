0ef46cefba51ba4f952447680831201f2e24b276
Gary Yang <gary.yang@titansoft.com.sg>
2020-06-07 02:12:58 +0800

    [KATA002] Dead Ants

---
+++ Kata_VS/Kata/Kata No.002 Dead ants/DeadAnts.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Kata
+{
+    public class DeadAnts
+    {
+        public int CountDeadAnts(string input)
+        {
+            if (InValidInput(input)) return 0;
+            input = RemoveCompleteAnts(input);
+            var antParts = CountAntParts(input);
+            return MaxAntParts(antParts);
+        }
+
+        private static int MaxAntParts(int[] antParts)
+        {
+            return Math.Max(Math.Max(antParts[0], antParts[1]), antParts[2]);
+        }
+
+        private static int[] CountAntParts(string input)
+        {
+            int[] antParts = new int[] { 0, 0, 0 };
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'a':
+                        antParts[0]++;
+                        break;
+
+                    case 'n':
+                        antParts[1]++;
+                        break;
+
+                    case 't':
+                        antParts[2]++;
+                        break;
+
+                    default: break;
+                }
+            }
+
+            return antParts;
+        }
+
+        private static string RemoveCompleteAnts(string input)
+        {
+            return input.Replace("ant", " ");
+        }
+
+        private static bool InValidInput(string ants)
+        {
+            return string.IsNullOrEmpty(ants);
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.002 Dead ants/DeadAntsTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass()]
+    public class DeadAntsTest
+    {
+        private DeadAnts _DeadAnts = new DeadAnts();
+
+        [TestMethod]
+        public void NoDeadAnts()
+        {
+            ShouldBeEqual(0, _DeadAnts.CountDeadAnts("ant.ant.ant.ant"));
+        }
+        [TestMethod]
+        public void NullInput()
+        {
+            ShouldBeEqual(0, _DeadAnts.CountDeadAnts(null));
+        }
+        [TestMethod]
+        public void TwoDeadAnts()
+        {
+            ShouldBeEqual(2, _DeadAnts.CountDeadAnts("ant.anantt.aantnt"));
+        }
+        [TestMethod]
+        public void OneDeadAnt()
+        {
+            ShouldBeEqual(1, _DeadAnts.CountDeadAnts("ant.ant....a.nt"));
+        }
+        [TestMethod]
+        public void ThreeDeadAnts()
+        {
+            ShouldBeEqual(3, _DeadAnts.CountDeadAnts("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t"));
+        }
+        [TestMethod]
+        public void A()
+        {
+            ShouldBeEqual(1, _DeadAnts.CountDeadAnts("a"));
+        }
+        [TestMethod]
+        public void N()
+        {
+            ShouldBeEqual(1, _DeadAnts.CountDeadAnts("n"));
+        }
+        [TestMethod]
+        public void T()
+        {
+            ShouldBeEqual(1, _DeadAnts.CountDeadAnts("t"));
+        }
+        [TestMethod]
+        public void NoAnts()
+        {
+            ShouldBeEqual(0, _DeadAnts.CountDeadAnts("...."));
+        }
+        [TestMethod]
+        public void EmptyInput()
+        {
+            ShouldBeEqual(0, _DeadAnts.CountDeadAnts(""));
+        }
+
+        private void ShouldBeEqual(int expected, int output)
+        {
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
