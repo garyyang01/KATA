e73b7486e1765e57e4e7c6dca452f528396b53ad
Gary Yang <gary.yang@titansoft.com.sg>
2020-06-07 03:06:26 +0800

    [KAT003] Band name generator

---
+++ Kata_VS/Kata/Kata No.003 Band name generator/BandNameGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata
+{
+    public class BandNameGenerator
+    {
+        public string GenerateBandName(string input)
+        {
+            if (InValidInput(input)) return "";
+            if (StartEndSameLetter(input))
+            {
+                string last = RemoveFirstLetter(input);
+                return RepeatBandNameRule(input, last);
+            }
+            else
+            {
+                return TheBandNameRule(input);
+            }
+        }
+
+        private static string RepeatBandNameRule(string input, string last)
+        {
+            return char.ToUpper(input[0]) + input.Substring(1) + last;
+        }
+
+        private static string RemoveFirstLetter(string input)
+        {
+            return (input.Remove(0, 1) == "") ? input : input.Remove(0, 1);
+        }
+
+        private static string TheBandNameRule(string input)
+        {
+            return "The " + char.ToUpper(input[0]) + input.Substring(1);
+        }
+
+        private static bool StartEndSameLetter(string str)
+        {
+            return str[0] == str[str.Length - 1];
+        }
+
+        private static bool InValidInput(string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.003 Band name generator/BandNameGeneratorTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass()]
+    public class BandNameGeneratorTest
+    {
+        private BandNameGenerator _BandNameGenerator = new BandNameGenerator();
+
+        [TestMethod]
+        public void Knife()
+        {
+            ShouldBeEqual("The Knife", _BandNameGenerator.GenerateBandName("knife"));
+        }
+        [TestMethod]
+        public void Tart()
+        {
+            ShouldBeEqual("Tartart", _BandNameGenerator.GenerateBandName("tart"));
+        }
+        [TestMethod]
+        public void Sandles()
+        {
+            ShouldBeEqual("Sandlesandles", _BandNameGenerator.GenerateBandName("sandles"));
+        }
+        [TestMethod]
+        public void Bed()
+        {
+            ShouldBeEqual("The Bed", _BandNameGenerator.GenerateBandName("bed"));
+        }
+        [TestMethod]
+        public void RepeatLetters()
+        {
+            ShouldBeEqual("Ttt", _BandNameGenerator.GenerateBandName("tt"));
+        }
+        [TestMethod]
+        public void EmptyInput()
+        {
+            ShouldBeEqual("", _BandNameGenerator.GenerateBandName(""));
+        }
+        [TestMethod]
+        public void NullInput()
+        {
+            ShouldBeEqual("", _BandNameGenerator.GenerateBandName(null));
+        }
+        [TestMethod]
+        public void SingleLetter()
+        {
+            ShouldBeEqual("Kk", _BandNameGenerator.GenerateBandName("k"));
+        }
+        private void ShouldBeEqual(string expected, string output)
+        {
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
