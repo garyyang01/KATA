46eda1a5c5c80844af8b904b6f290cd86543b034
Gary Yang <gary.yang@titansoft.com.sg>
2020-06-22 02:03:45 +0800

    [KATA004] Counting Duplicates

---
+++ Kata_VS/Kata/Kata No.004 Counting Duplicates/DuplicatesCounter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    public class DuplicatesCounter
+    {
+        public int CountDuplicates(string input)
+        {
+            if (IsInValidInput(input)) return 0;
+            var ans = CountDuplicateCharater(input);
+            return ans;
+        }
+
+        private static int CountDuplicateCharater(string input)
+        {
+            var existedSet = new HashSet<char>();
+            var duplicateCountedSet = new HashSet<char>();
+            int ans = 0;
+            foreach (var alpha in input.ToLower())
+            {
+                if (existedSet.Contains(alpha))
+                {
+                    if (!duplicateCountedSet.Contains(alpha))
+                    {
+                        duplicateCountedSet.Add(alpha);
+                        ans++;
+                    }
+
+                    continue;
+                }
+
+                existedSet.Add(alpha);
+            }
+
+            return ans;
+        }
+
+        private static bool IsInValidInput(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.004 Counting Duplicates/DuplicatesCounterTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass()]
+    public class DuplicatesCounterTest
+    {
+        private DuplicatesCounter _duplicatesCounter = new DuplicatesCounter();
+
+        [TestMethod]
+        public void Empty()
+        {
+            ShouldBeEqual(0, _duplicatesCounter.CountDuplicates(""));
+        }
+
+        [TestMethod]
+        public void Null()
+        {
+            ShouldBeEqual(0, _duplicatesCounter.CountDuplicates(null));
+        }
+
+        [TestMethod]
+        public void KataTest1()
+        {
+            ShouldBeEqual(0, _duplicatesCounter.CountDuplicates("abcde"));
+        }
+
+        [TestMethod]
+        public void KataTest2()
+        {
+            ShouldBeEqual(2, _duplicatesCounter.CountDuplicates("aabbcde"));
+        }
+
+        [TestMethod]
+        public void DifferentCase()
+        {
+            ShouldBeEqual(2, _duplicatesCounter.CountDuplicates("aabBcde"));
+        }
+
+        [TestMethod]
+        public void KataTest3()
+        {
+            ShouldBeEqual(1, _duplicatesCounter.CountDuplicates("Indivisibility"));
+        }
+
+        [TestMethod]
+        public void NotBeAdjacent()
+        {
+            ShouldBeEqual(2, _duplicatesCounter.CountDuplicates("Indivisibilities"));
+        }
+
+        [TestMethod]
+        public void Number()
+        {
+            ShouldBeEqual(4, _duplicatesCounter.CountDuplicates("123asd321zxcd"));
+        }
+
+        [TestMethod]
+        public void MarkChar()
+        {
+            ShouldBeEqual(4, _duplicatesCounter.CountDuplicates(",./..,,l;21sad,./zxc1"));
+        }
+
+        private void ShouldBeEqual(int expected, int output)
+        {
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
