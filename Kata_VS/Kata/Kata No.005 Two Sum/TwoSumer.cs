7f1341075faedd7c7addbe88c533a75d763a1750
Gary Yang <gary.yang@titansoft.com.sg>
2020-06-22 04:02:33 +0800

    [KATA005] Two Sum

---
+++ Kata_VS/Kata/Kata No.005 Two Sum/TwoSumer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    public class TwoSumer
+    {
+        public int[] GetSumTwo(int[] numArray, int target)
+        {
+            if (IsInValidInput(numArray, target)) return null;
+            List<int> numList = numArray.ToList();
+            int[] ans = new int[2];
+            foreach (var oneNum in numList)
+            {
+                var index = numList.IndexOf(oneNum);
+                var twoNum = target - oneNum;
+                if (FoundAnotherIndex(numList, twoNum, index))
+                {
+                    return SetUpAns(ans, numList, oneNum, twoNum);
+                }
+            }
+            return null;
+        }
+
+        private static int[] SetUpAns(int[] ans, List<int> numList, int oneNum, int twoNum)
+        {
+            ans[0] = numList.IndexOf(oneNum);
+            ans[1] = numList.IndexOf(twoNum, numList.IndexOf(oneNum) + 1);
+            return ans;
+        }
+
+        private static bool FoundAnotherIndex(List<int> numList, int twoNum, int index)
+        {
+            return numList.IndexOf(twoNum, index + 1) >= 0;
+        }
+
+        private static bool IsInValidInput(int[] numArray, int target)
+        {
+            return numArray == null || numArray.Length < 2 || target == null;
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.005 Two Sum/TwoSumerTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass()]
+    public class TwoSumerTest
+    {
+        private TwoSumer _TwoSumer = new TwoSumer();
+
+        [TestMethod]
+        public void NormalCase()
+        {
+            ShouldBeEqual(new[] { 0, 1 }, _TwoSumer.GetSumTwo(new[] { 2, 7, 11, 15 }, 9));
+        }
+
+        [TestMethod]
+        public void CheckCanNotFoundDuplicatTwoSum()
+        {
+            ShouldBeEqual(new[] { 1, 2 }, _TwoSumer.GetSumTwo(new[] { 3, 2, 4 }, 6));
+        }
+
+        [TestMethod]
+        public void DuplicateNum()
+        {
+            ShouldBeEqual(new[] { 0, 1 }, _TwoSumer.GetSumTwo(new[] { 3, 3 }, 6));
+        }
+
+        [TestMethod]
+        public void CanNotFoundTwoSum()
+        {
+            ShouldBeEqual(null, _TwoSumer.GetSumTwo(new[] { 3, 3 }, 7));
+        }
+
+        [TestMethod]
+        public void NullInput()
+        {
+            ShouldBeEqual(null, _TwoSumer.GetSumTwo(null, 0));
+        }
+
+        [TestMethod]
+        public void EmptyInput()
+        {
+            ShouldBeEqual(null, _TwoSumer.GetSumTwo(new int[] { }, 0));
+        }
+
+        [TestMethod]
+        public void OnlyOneNum()
+        {
+            ShouldBeEqual(null, _TwoSumer.GetSumTwo(new[] { 3 }, 0));
+        }
+
+        [TestMethod]
+        public void NagativeInput()
+        {
+            ShouldBeEqual(new[] { 1, 2 }, _TwoSumer.GetSumTwo(new[] { 1, -3, 4 }, 1));
+        }
+
+        [TestMethod]
+        public void NagativeTarget()
+        {
+            ShouldBeEqual(new[] { 0, 1 }, _TwoSumer.GetSumTwo(new[] { 1, -3, 4 }, -2));
+        }
+
+        private void ShouldBeEqual(int[] expected, int[] output)
+        {
+            if (expected == null)
+            {
+                Assert.AreEqual(expected, output, "Null not equal");
+            }
+            else
+            {
+                Assert.AreEqual(expected.Length, output.Length, "array length not equal");
+                Assert.AreEqual(expected[0], output[0], "oneNum not equal");
+                Assert.AreEqual(expected[1], output[1], "twoNum not equal");
+            }
+        }
+    }
+}
