7c274809da32037c1cc8747e272194d842f7c052
Gary Yang <gary.yang@titansoft.com.sg>
2020-07-13 03:54:44 +0800

    [KATA006] ThreeSum

---
+++ Kata_VS/Kata/Kata No.006 Three Sum/ThreeSumer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    public class ThreeSumer
+    {
+        public List<List<int>> GetSumThree(int[] inputArray)
+        {
+            if (IsInValidInput(inputArray))
+            {
+                return null;
+            }
+            var numQueue = new Queue<int>(inputArray);
+            List<List<int>> ansList = new List<List<int>>();
+            while (numQueue.Count > 2)
+            {
+                var firstNum = numQueue.Dequeue();
+                var tempQueue = new Queue<int>(numQueue);
+                while (tempQueue.Count > 1)
+                {
+                    var secondNum = tempQueue.Dequeue();
+                    if (tempQueue.Contains(-firstNum - secondNum))
+                    {
+                        var ansArray = GetAnsArray(firstNum, secondNum);
+                        if (IsNotExist(ansList, ansArray))
+                        {
+                            ansList.Add(ansArray);
+                        }
+                    }
+                }
+            }
+            return ansList.Distinct().ToList();
+        }
+
+        private static List<int> GetAnsArray(int x, int y)
+        {
+            var tempArray = new[] { x, y, -x - y }.ToList();
+            tempArray.Sort();
+            return tempArray;
+        }
+
+        private static bool IsNotExist(List<List<int>> ansList, List<int> ansArray)
+        {
+            return !ansList.Exists(ans => ans.SequenceEqual(ansArray));
+        }
+
+        private static bool IsInValidInput(int[] numArray)
+        {
+            return numArray == null || numArray.Length < 3;
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.006 Three Sum/ThreeSummerTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    [TestClass()]
+    public class ThreeSummerTest
+    {
+        private ThreeSumer _ThreeSumer = new ThreeSumer();
+
+        [TestMethod]
+        public void NormalCase()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            expected.Add(new[] { -1, 0, 1 }.ToList());
+            expected.Add(new[] { -1, -1, 2 }.ToList());
+            ShouldBeEqual(expected, _ThreeSumer.GetSumThree(new[] { -1, 0, 1, 2, -1, -4 }));
+        }
+
+        [TestMethod]
+        public void SameNumCase()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            expected.Add(new[] { 0, 0, 0 }.ToList());
+            ShouldBeEqual(expected, _ThreeSumer.GetSumThree(new[] { 0, 0, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void DuplicateCase()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            expected.Add(new[] { -1, 0, 1 }.ToList());
+            ShouldBeEqual(expected, _ThreeSumer.GetSumThree(new[] { -1, 0, 1, -1, 0, 1 }));
+        }
+
+        [TestMethod]
+        public void NoAnswer()
+        {
+            List<List<int>> expected = new List<List<int>>();
+            ShouldBeEqual(expected, _ThreeSumer.GetSumThree(new[] { -1, -1, -1, -1 }));
+        }
+
+        [TestMethod]
+        public void LessThanThreeNum()
+        {
+            ShouldBeEqual(null, _ThreeSumer.GetSumThree(new[] { -1, 1 }));
+        }
+
+        [TestMethod]
+        public void NullInput()
+        {
+            ShouldBeEqual(null, _ThreeSumer.GetSumThree(null));
+        }
+
+        private void ShouldBeEqual(List<List<int>> expected, List<List<int>> output)
+        {
+            if (expected == null)
+            {
+                Assert.AreEqual(expected, output, "Null not equal");
+                return;
+            }
+            Assert.AreEqual(expected.Count, output.Count, "Ans Num Should equal");
+            for (int i = 0; i < output.Count; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i], $"expected : {expected[i]} \r\n output : {output[i]}");
+            }
+        }
+    }
+}
