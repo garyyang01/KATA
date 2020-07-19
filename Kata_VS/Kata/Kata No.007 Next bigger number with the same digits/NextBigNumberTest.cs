ff964eb237cf4272ebea661df1231349af83dc31
Gary Yang <gary.yang@titansoft.com.sg>
2020-07-19 22:51:19 +0800

    [KATA007] Next bigger number with the same digits

---
+++ Kata_VS/Kata/Kata No.007 Next bigger number with the same digits/NextBigNumberTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata
+{
+    [TestClass()]
+    public class NextBigNumberTest
+    {
+        private SameDigitCaculater _sameDigitCaculater = new SameDigitCaculater();
+
+        [TestMethod]
+        public void NormalCase1()
+        {
+            ShouldBeEqual(21, _sameDigitCaculater.GetBigNumber(12));
+        }
+
+        [TestMethod]
+        public void NormalCase2()
+        {
+            ShouldBeEqual(531, _sameDigitCaculater.GetBigNumber(513));
+        }
+
+        [TestMethod]
+        public void NormalCase3()
+        {
+            ShouldBeEqual(1423, _sameDigitCaculater.GetBigNumber(1342));
+        }
+
+        [TestMethod]
+        public void NormalCase4()
+        {
+            ShouldBeEqual(414, _sameDigitCaculater.GetBigNumber(144));
+        }
+
+        [TestMethod]
+        public void NormalCase5()
+        {
+            ShouldBeEqual(908, _sameDigitCaculater.GetBigNumber(890));
+        }
+
+        [TestMethod]
+        public void NormalCase6()
+        {
+            ShouldBeEqual(9119, _sameDigitCaculater.GetBigNumber(1991));
+        }
+
+        [TestMethod]
+        public void DuplicateNumber()
+        {
+            ShouldBeEqual(-1, _sameDigitCaculater.GetBigNumber(555));
+        }
+
+        [TestMethod]
+        public void OneDigit()
+        {
+            ShouldBeEqual(-1, _sameDigitCaculater.GetBigNumber(1));
+        }
+
+        [TestMethod]
+        public void NegativeNumber()
+        {
+            ShouldBeEqual(-1, _sameDigitCaculater.GetBigNumber(-124));
+        }
+
+        [TestMethod]
+        public void Zero()
+        {
+            ShouldBeEqual(-1, _sameDigitCaculater.GetBigNumber(0));
+        }
+
+        [TestMethod]
+        public void ZeroFirst()
+        {
+            ShouldBeEqual(21, _sameDigitCaculater.GetBigNumber(0012));
+        }
+
+        [TestMethod]
+        public void BigNumber()
+        {
+            ShouldBeEqual(3321545083902189405, _sameDigitCaculater.GetBigNumber(3321545083902189054));
+        }
+
+        private void ShouldBeEqual(long expected, long output)
+        {
+            Assert.AreEqual(expected, output, "not equal");
+        }
+    }
+}
---
+++ Kata_VS/Kata/Kata No.007 Next bigger number with the same digits/SameDigitCaculater.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata
+{
+    public class SameDigitCaculater
+    {
+        public long GetBigNumber(long inputNum)
+        {
+            if (IsInValidInput(inputNum))
+            {
+                return -1;
+            }
+
+            var digitList = inputNum.ToString();
+            for (int i = digitList.Length - 1; i > 0; i--)
+            {
+                if (digitList[i] > digitList[i - 1])
+                {
+                    var leftPart = digitList.Substring(0, i - 1);
+                    var key = digitList[i - 1];
+                    var rightPart = digitList.Substring(i);
+                    var newKey = GetNewKey(rightPart, key, out var newKeyIndex);
+                    var newRightPart = GetNewRightPart(rightPart, newKeyIndex, key);
+                    return SetAns(leftPart, newKey, newRightPart);
+                }
+            }
+            return -1;
+        }
+
+        private static long SetAns(string leftPart, char newKey, List<char> newRightPart)
+        {
+            string ans = leftPart + newKey + ConvertCharListToString(newRightPart);
+            return Convert.ToInt64(ans);
+        }
+
+        private static string ConvertCharListToString(List<char> newRightPart)
+        {
+            return new string(newRightPart.ToArray());
+        }
+
+        private static List<char> GetNewRightPart(string rightPart, int newKeyIndex, char key)
+        {
+            var newRightPart = rightPart.ToList();
+            newRightPart[newKeyIndex] = key;
+            newRightPart.Sort();
+            return newRightPart;
+        }
+
+        private static char GetNewKey(string rightPart, char key, out int newKeyIndex)
+        {
+            var newKey = '9';
+            newKeyIndex = 0;
+            for (int j = 0; j < rightPart.Length; j++)
+            {
+                if (rightPart[j] > key && rightPart[j] <= newKey)
+                {
+                    newKey = rightPart[j];
+                    newKeyIndex = j;
+                }
+            }
+
+            return newKey;
+        }
+
+        private static bool IsInValidInput(long inputNum)
+        {
+            return inputNum <= 11 || inputNum == null;
+        }
+    }
+}
